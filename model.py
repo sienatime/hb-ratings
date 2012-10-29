bf22328d40a101c6262403a71ad6170d55e046df
chriszf <chriszf@gmail.com>
2012-10-29 12:39:18 -0700

    Fixing readme

--- model.py
+++ model.py
@@ -1,11 +1,49 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+engine = create_engine("sqlite:///ratings.db", echo = False)
+session = scoped_session(sessionmaker(bind = engine, autocommit=False, autoflush=False))
 
 Base = declarative_base()
+Base.query = session.query_property()
 
 ### Class declarations go here
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer(), primary_key = True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer(),nullable = True)
+    zipcode = Column(String(15), nullable=True)
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer(), primary_key = True)
+    movie_title = Column(String(64), nullable = True)
+    release_date = Column(DateTime(), nullable = True)
+    imdb_url = Column(String(100), nullable=True)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer(), primary_key = True)
+    user_id = Column(Integer(), ForeignKey('users.id'), nullable=True)
+    movie_id = Column(Integer(), ForeignKey('movies.id'), nullable=True)
+    rating = Column(Integer(), nullable=True)
+    timestamp = Column(DateTime(), nullable=True)
+
+    user = relationship("User",
+        backref=backref("ratings", order_by=id))
+
+    movie = relationship("Movie",
+        backref=backref("ratings", order_by=id))
 
 ### End class declarations
 
--- seed.py
+++ seed.py
@@ -1,21 +1,60 @@
 import model
 import csv
+import datetime
 
 def load_users(session):
     # use u.user
-    pass
+    f = open("seed_data/u.user")
+    r = csv.reader(f, delimiter="|")
+
+    for row in r:
+        #do stuff
+        data = row 
+        #this is a list of data
+        user = model.User(age= data[1], zipcode= data[4])
+        session.add(user)
+    session.commit()
 
 def load_movies(session):
     # use u.item
-    pass
+    f = open("seed_data/u.item")
+    r = csv.reader(f, delimiter="|")
+
+    for row in r:
+        data = row
+        title = data[1]
+        tokens = title.split("(")
+        new_title = tokens[0].strip()
+        decoded_title = new_title.decode("latin-1")
+        rel_date =  data[2]
+
+        if rel_date:
+            new_date = datetime.datetime.strptime(rel_date, "%d-%b-%Y")
+            movie = model.Movie(movie_title = decoded_title, release_date = new_date, imdb_url = data[4])
+        else:
+            movie = model.Movie(movie_title = decoded_title, imdb_url = data[4])
+        
+        session.add(movie)
+    session.commit()
 
 def load_ratings(session):
     # use u.data
-    pass
+    f = open("seed_data/u.data")
+    r = csv.reader(f, delimiter= "\t")
+
+    for row in r:
+        data = row
+        time = datetime.date.fromtimestamp(float(data[3]))
+        rating = model.Rating(user_id = data[0], movie_id = data[1], rating = data[2], timestamp = time)
+        session.add(rating)
+    session.commit()
+
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    load_movies(session)
+    load_ratings(session)
 
 if __name__ == "__main__":
     s= model.connect()
