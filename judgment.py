be781d1f711dc82f8c46a8b921ee616087ed4b93
Hackbright Student <user@chromebox-002.(none)>
2013-10-28 18:00:15 -0700

    Nearly complete with section 2.

--- judgment.py
+++ judgment.py
@@ -1,11 +1,83 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for, session
 import model
 app = Flask(__name__)
+app.secret_key = "yosecretkey"
 
 @app.route("/")
 def index():
-    user_list = model.session.query(model.User).limit(5).all()
+    if session.get('email'):
+        return render_template("home.html")
+    else:
+        return redirect(url_for("register"))
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/register", methods=['POST'])
+def new_user():
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+    verify_pwd = request.form.get("password_verify")
+    age = request.form.get("age")
+    zipcode = request.form.get("zipcode")
+
+    if (pwd == verify_pwd) and (email and pwd and age and zipcode):
+        if model.is_user(email):
+            flash("You're already registered. Please sign in.")
+            return redirect(url_for("signin"))
+        else:
+            model.add_user(email, pwd, age, zipcode)
+            redirect(url_for("signin"))
+    elif pwd != verify_pwd:
+        flash("Passwords must match.","error")
+        return redirect(url_for("register"))
+    else:
+        flash("All fields are required.","error")
+        return redirect(url_for("register"))
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
+
+@app.route("/signin", methods=['POST'])
+def sign_in():
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+
+    if model.auth_match(email, pwd):
+        flash("Signed in.")
+        session['email'] = email
+        session['user_id'] = model.get_id_by_email(email)
+        return render_template("home.html")
+    else:
+        flash("Your email and password didn't match our records. Please sign in.")
+        return redirect(url_for("signin"))
+
+@app.route("/all_users")
+def get_users():
+    user_list = model.get_all_users()
     return render_template("user_list.html", users=user_list)
+
+@app.route("/user/<user_id>")
+def get_profile(user_id):
+    ratings = model.get_ratings_from_user(user_id)
+    return render_template("ratings.html", ratings=ratings, profile=user_id)
+
+@app.route("/movie/<movie_id>")
+def show_movie(movie_id):
+    ratings = model.get_ratings_for_movie(movie_id)
+    movie_title = model.get_movie_title_from_id(movie_id)
+
+    all_sum = 0
+    count = 0
+    for rating in ratings:
+        all_sum += rating.rating
+        count += 1
+
+    avg = float(all_sum)/count
+
+    return render_template("movie.html", avg=avg, movie_title=movie_title)
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
+import hashlib
 
 engine = create_engine("sqlite:///ratings.db", echo = False)
 session = scoped_session(sessionmaker(bind = engine, autocommit=False, autoflush=False))
@@ -46,6 +47,46 @@ class Rating(Base):
         backref=backref("ratings", order_by=id))
 
 ### End class declarations
+
+def add_user(inp_email, inp_password, inp_age, inp_zipcode):
+    user = User(email=inp_email, password=md5_hash(inp_password), age=inp_age, zipcode=inp_zipcode)
+    session.add(user)
+    session.commit()
+
+def is_user(inp_email):
+    user = session.query(User).filter_by(email=inp_email).all()
+    return user
+
+def md5_hash(password):
+    return hashlib.md5(password).hexdigest()
+
+def auth_match(inp_email, inp_password):
+    user = session.query(User).filter_by(email=inp_email, password=md5_hash(inp_password)).one()
+    return user
+
+def get_id_by_email(inp_email):
+    user = session.query(User).filter_by(email=inp_email).one()
+    return user.id
+
+def get_email_by_id(inp_id):
+    email = session.query(User).filter_by(id=inp_id).one()
+    return email
+
+def get_ratings_from_user(userid):
+    ratings = session.query(Rating).filter_by(user_id=userid).all()
+    return ratings
+
+def get_ratings_for_movie(inp_movie):
+    ratings = session.query(Rating).filter_by(movie_id=inp_movie).all()
+    return ratings
+
+def get_movie_title_from_id(inp_movie_id):
+    movie_title = session.query(Movie).filter_by(id=inp_movie_id).one()
+    return movie_title.movie_title
+
+def get_all_users():
+    users = session.query(User).limit(5).all()
+    return users
 
 def main():
     """In case we need this for something"""
